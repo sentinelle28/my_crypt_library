6f31c4b8c3fda9c765245a114930f1527ceec157
sentinelle28 <augustesoulie@gmail.com>
2024-06-15 15:52:52 +0200

    fixed bug

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from tkinter import *
 from tkinter.ttk import *
+
+#function
+def generate_key():
+    if crypt_drop_down.get() != "":
+        key_input.delete(1.0,END)
+        key_input.insert(END,"test")
+
+#object       
 screen = Tk(screenName="crypt app")
 screen.geometry("600x700")
 
@@ -7,7 +15,15 @@ crypt_drop_down = Combobox(
     state="readonly",
     value= ["test","new"]
 )
+key_input = Text(height=5,width=20)
+
+#placing things up
 crypt_drop_down.place(x=100,y=200)
+key_input.place(x=200,y=300)
+
+generate_key_button = Button(command=generate_key,text="generate a key").place(x=200,y=200)
+
+
 
 
 
