9988d2ec55527602210c4a094239f854585d3f1b
sentinelle28 <augustesoulie@gmail.com>
2024-06-15 14:50:17 +0200

    just add a dropdown

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from tkinter import *
-
+from tkinter.ttk import *
 screen = Tk(screenName="crypt app")
+screen.geometry("600x700")
+
+crypt_drop_down = Combobox(
+    state="readonly",
+    value= ["test","new"]
+)
+crypt_drop_down.place(x=100,y=200)
+
 
 
 mainloop()
