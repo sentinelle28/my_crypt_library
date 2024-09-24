281da7a4cdbeadcd32ff5e634ff1993744595ef8
sentinelle28 <102462227+sentinelle28@users.noreply.github.com>
2024-09-24 13:14:50 +0200

    added better support
    we miss the key brah

--- cryptage/advanced_subsitution_with_number.py
+++ cryptage/advanced_subsitution_with_number.py
@@ -1,14 +1,11 @@
 import random
 
 
-def Generate(list_to_cypher):
-    global list_of_letter
-    global key
-    global referencement
-    global element_to_put
-    element_to_put =  0
-    list_of_letter = []
-    key = []
+
+def Generate(list_to_cypher:str):
+    element_to_put:int =  0
+    list_of_letter:list = []#:list[str]
+    key:list = []#list[int]
     for letter in list_to_cypher:
         if not letter in list_of_letter:
             list_of_letter.append(letter)
@@ -28,30 +25,32 @@ def Generate(list_to_cypher):
         if not it in key:
             element_to_put = it
             break
+    return key,list_of_letter,referencement,element_to_put
     
-def Cypher(list_to_cypher,key,list_of_letter):
-    global list_cyphered
+def Cypher(list_to_cypher:list,key:list,list_of_letter:list,referencement:int):
     list_cyphered = []
     for i in range(len(list_to_cypher)):
         search = key[list_of_letter.index(list_to_cypher[i])] + (referencement + (i + 1))
         list_cyphered.append(search)
+    return list_cyphered
         
-def Decypher(list_to_decypher,key,list_of_letter):
-    global list_decyphered
+def Decypher(list_to_decypher:list,key:list,list_of_letter:list,referencement:int):
     list_decyphered = []
     for i in range(len(list_to_decypher)):
+        print(list_to_decypher[i])
         search = list_to_decypher[i] - (referencement + (i + 1))
         list_decyphered.append(list_of_letter[key.index(search)])
+    return list_decyphered
  
-def normalised_key(object_list,element_to_put):
+def normalised_key(object_list,element_to_put:str):
     normalised_key_text = ""
     for object in object_list:
         normalised_key_text += str(object)
         normalised_key_text += str(element_to_put)
     return normalised_key_text
        
-def unormalized(text,element_to_put):
-    extracted_list = []
+def unormalized(text:str,element_to_put:str):
+    extracted_list = [] #list[int]
     elements = text.split(str(element_to_put))
     for element in elements:
         if element:
@@ -64,7 +63,8 @@ def assemble_text(liste):
     for i in liste:
         text+= i
     return text
-    
+ 
+"""
 while True:
     choice = input("Advanced subsitution with number cryptage \n 1:Generate the Key \n 2:Cyphered the text \n 3:Decyphered a text \n 4: Unnormalized key \n 5: quit \nyour choice:")
     if choice.isnumeric():
@@ -121,8 +121,26 @@ while True:
      
 
 
+
+"""  
+
 #normalised key:  13/5/15/20/8/11/9/6/1/4/23/10/18/22/16/24/7/3/19/17/14/2/12/
 #normalised letter key:  y/o/u/ /s/c/e/f/l/n/r/p/t/h/i/m/a/g/k/:/I/b/w/
 #third key:43
-#Fourth key: 21
-    
+#Fourth key: 21   
+
+
+def extract_key(text:str,element_to_put:str):        
+    return text.split(str(element_to_put)) 
+
+key_1 = unormalized("13/5/15/20/8/11/9/6/1/4/23/10/18/22/16/24/7/3/19/17/14/2/12/","/") #list[int]
+key_2 = extract_key("y/o/u/ /s/c/e/f/l/n/r/p/t/h/i/m/a/g/k/:/I/b/w/","/") #list[str]
+key_3 = 43 #int
+key_4 = 18 #int
+
+#message = "258608778168818081808242820481308154821082888391819887682008378826481618360830083648297819682908330874489684958408849083608407826687028120845182108430848487658460851781104858881100811738572831889728121081064862781392810628126081464824881134813448156087268938812248207877084978728116881110875088368770814828869811208145881312814948159681870818068870896883568198089108193281023818881140896081067849089908110087078153081751814568315810608224782268810908121081887811208259981254820708812822238259681785825208278381342861581240813758239483818192081548811708131081452829268281482430828568287781380823638308083243832668314685768232081460816178296817888150081661824328306082310832558171682669815808174983840828988340283912865682970834868400881848830428187081197830968207684002817508193682301835608214883780818108200281830855285558186082244856482079824708191082112811588582835108274482758819808" 
+message = "1501877182418521884181351864181021821618304181801842188818322182401822518182182971833618464184801893182881856118306181751839618481183041811718401841182941860218308184051859818658184321863718300182551872818"
+message_1 = unormalized(message,str(key_4)) #list[int] 1/4
+print(message_1)
+message_2 = Decypher(message_1,key_1,key_2,key_3) #4/4
+print(message_2)
